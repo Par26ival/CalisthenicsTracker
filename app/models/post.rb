10592fa6e2bf520cb9dd976994c25c7e720cd6ef
Par26ival <kal.tomov@gmail.com>
2025-12-27 11:03:22 +0000

    Users can now add files (photos or videos) to the posts. Also tried to fix majour github corruption, i dont know if this will commit properly or at all

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,21 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  has_many_attached :media
+
   validates :content, presence: true, length: { maximum: 1000 }
+
+  validate :media_type_and_size
+
+  private
+
+  def media_type_and_size
+    media.each do |file|
+      if !file.content_type.in?(%w[image/jpeg image/png image/webp video/mp4 video/webm])
+        errors.add(:media, "must be an image or video")
+      elsif file.byte_size > 50.megabytes
+        errors.add(:media, "is too large (max 50MB)")
+      end
+    end
+  end
 end
